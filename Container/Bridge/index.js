e8bb1751fa1df3d933a89a89fc41002c2595d2a3
YasarArafath <yasar.arafath@thoughthub.net>
2023-03-17 09:14:43 +0530

    BridgeContentUpdate-V1

--- Container/Bridge/index.js
+++ Container/Bridge/index.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-import { TextInput, TouchableOpacity, View } from "react-native";
+import { Platform, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Fonts from "../../Themes/Fonts";
 import { globalstyle } from "../../Themes/GlobalStyle";
 import { styles } from "./styles";
-import Send from '../../assets/Svg/send.svg'
+import SendLogo from '../../assets/Svg/send.svg'
 export default function Bridge() {
     const [Name, setName] = useState('')
+
+    const Send=()=>{
+
+    }
     return (
         <View style={[globalstyle.container,styles.CentreDisabled]}>
             <View style={styles.SearchandSortContainer}>
@@ -14,16 +18,18 @@ export default function Bridge() {
                         placeholder="Enter Name"
                         style={[Fonts.SIZES.h6, Fonts.COLORS.black, { flex: 1 }]}
                         value={Name}
-                        onChangeText={(txt) => SearchName(txt)}
+                        onChangeText={(txt) => setName(txt)}
                     />
                 </View>
                 <TouchableOpacity style={styles.SendButton}
-                    // onPress={Sort}
+                    onPress={Send}
                 >
-                    <Send/>
+                    <SendLogo/>
                 </TouchableOpacity>
 
             </View>
+
+            <Text style={[Fonts.COLORS.labelgreen,Fonts.SIZES.h1,globalstyle.mt30,{alignSelf:'center'}]}>From {Platform.OS==="ios"?"iOS":"Android"} :</Text>
         </View>
     )
 }
